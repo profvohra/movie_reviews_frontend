9be3f23f8c92b3c618502fb2daf6f7934930e1dd
Rosemina Vohra <rosemina.vohra@gmail.com>
2024-08-23 17:26:04 -0400

    Chapter 13 React.js Frontend

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import { Routes, Route, NavLink } from "react-router-dom";
+//import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h3>Hello World</h3>
+    </div>
+  );
+}
+
+export default App;
