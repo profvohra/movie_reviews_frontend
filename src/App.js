fd973912df042755367dc9acb92d3dd82d20c8a1
Rosemina Vohra <rosemina.vohra@gmail.com>
2024-08-23 18:08:57 -0400

    Chapter 14 React.js Navigation Bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import React, { useState } from "react";
 import { Routes, Route, NavLink } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import AddReview from "./components/addReview";
+import MoviesList from "./components/moviesList";
+import Movie from "./components/movie";
+import Login from "./components/login";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  async function login(user = null) {
+    setUser(user);
+  }
+
+  async function logout() {
+    setUser(null);
+  }
   return (
     <div className="App">
-      <h2>Hello World</h2>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand>Movie Reviews</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link as={NavLink} to={"/movies"}>
+              Movies
+            </Nav.Link>
+            <Nav.Link as={NavLink} to={user ? "" : "/login"}>
+              {user ? "Logout User" : "Login"}
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 }
