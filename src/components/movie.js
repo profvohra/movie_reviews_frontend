1fa1ddc41d0f5b472683787af532fc7b2a5c5592
Rosemina Vohra <rosemina.vohra@gmail.com>
2025-08-19 11:49:17 -0400

    Chapter22

--- src/components/movie.js
+++ src/components/movie.js
@@ -34,6 +34,21 @@ const Movie = (props) => {
     getMovie(id)
   }, [id])
 
+  const deleteReview = (reviewId, index) => {
+    MovieDataService.deleteReview(reviewId, props.user.id)
+      .then(response => {
+        setMovie((prevState) => {
+          prevState.reviews.splice(index, 1)
+          return ({
+            ...prevState
+          })
+        })
+      })
+      .catch(e => {
+        console.log(e)
+      })
+  }
+
   return (
     <div>
       <Container>
@@ -69,7 +84,11 @@ const Movie = (props) => {
                           state={{ currentReview: review }}
                         >Edit</Link>
                         </Col>
-                        <Col><Button variant="link">Delete</Button></Col>
+                        <Col>
+                          <Button variant="link" onClick={() => deleteReview(review._id, index)}>
+                            Delete
+                          </Button>
+                        </Col>
                       </Row>}
                   </Card.Body>
                 </Card>
